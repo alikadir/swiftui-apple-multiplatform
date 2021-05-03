6e7ce5fb7b1e95168ecc28a632a8a24fab27fb7a
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-05-03 16:58:14 +0300

    used vStack, image, textField, text, stepper, alert

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name = ""
+    @State var showResetAlert = false
+    @State var imageSize:CGFloat = 150
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("square")
+                .resizable()
+                .frame(width: imageSize, height: imageSize, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .cornerRadius(8.0)
+                .padding()
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            
+            Stepper(value: $imageSize, in: 150...320, step: 10){
+            }.padding().padding(.bottom, 50)
+            
+            TextField("Your Name...", text: $name).textFieldStyle(RoundedBorderTextFieldStyle())
+                .fixedSize()
+            
+            Text("Hi, \(name) How are you?")
+                .padding()
+            
+            Button("Reset Name",action:{
+                name = "";
+                showResetAlert.toggle();
+            }).padding()
+            
+            
+        }.alert(isPresented: $showResetAlert) { () -> Alert in Alert(title: Text("Reseted Name"), message: Text("Reset Name Succeded"))
+        }
+        
     }
 }
 
@@ -19,3 +48,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
