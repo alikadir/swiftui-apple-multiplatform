fe013707b690e388fe440318b72c96757d077a09
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-05-03 16:40:44 +0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Ali Kadir BAGCIOGLU on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/swiftui_apple_multiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_apple_multiplatformApp.swift
+//  Shared
+//
+//  Created by Ali Kadir BAGCIOGLU on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_apple_multiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
